37fb2c30fa0ac78c533c9c0a541ebe53dee27e91
Miroslav Galabov <wolfnature13@gmail.com>
2017-11-30 13:45:29 +0200

    Created model interfaces - IJsonNode and IJsonValue. Issue #3

---
+++ JsonTools/Interfaces/IJsonNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JsonTools.Interfaces
+{
+    public interface IJsonNode
+    {
+        string Key { get; }
+
+        IJsonNode ParentNode { get; }
+
+        IJsonValue this[string key] { get; set; }
+
+        IEnumerable<string> Keys { get; }
+
+        IEnumerable<IJsonValue> Values { get; }
+
+        bool KeyExists(string key);
+    }
+}
---
+++ JsonTools/Interfaces/IJsonValue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JsonTools.Interfaces
+{
+    public interface IJsonValue
+    {
+        object InnerValue { get; }
+
+        Type InnerType { get; }
+
+        bool IsString();
+
+        bool IsNode();
+    }
+}
